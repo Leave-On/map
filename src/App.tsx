f2d6657d30bd79b7100fffe4e9f1b5b37c7e489c
Levon Sarkisyan <leva679@gmail.com>
2023-08-05 16:14:30 +0300

    map inited

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css'
-// import { MainLayout } from './app/style/layouts/MainLayout'
+
 import { MapComponent } from './components/map/MapComponent'
-// import { Toolbar } from './components/toolbar/Toolbar'
 
 function App() {
 
   return (
-    // <MainLayout
-    //   left={<Toolbar />}
-    //   right={<MapComponent />}
-    // />
     <MapComponent />
   )
 }
--- src/components/map/MapComponent.tsx
+++ src/components/map/MapComponent.tsx
@@ -1,20 +1,25 @@
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import { FC } from "react";
-
+import cls from "./MapComponent.module.scss";
 export const MapComponent: FC = () => {
-
-
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={8} scrollWheelZoom={false}>
+    <div className={cls.MapComponent}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={8}
+        scrollWheelZoom={false}
+        className={cls.MapComponent}
+      >
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
